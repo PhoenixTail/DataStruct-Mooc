af321408b77ed0afbd358ed72d510fe4772ba718
zh_x <329829187@qq.com>
2017-10-13 16:31:04 +0800

    add tree operation

---
+++ 04-tree 7/BinTree.c
@@ -0,0 +1,29 @@
+/*
+typedef int ElementType;
+typedef struct TNode *Position;
+typedef Position BinTree;
+struct TNode{
+    ElementType Data;
+    BinTree Left;
+    BinTree Right;
+};
+*/
+
+BinTree Insert( BinTree BST, ElementType X )
+{
+    BinTree root_ptr = NULL;
+    if(!BinTree)
+}
+BinTree Delete( BinTree BST, ElementType X )
+{
+}
+Position Find( BinTree BST, ElementType X )
+{
+}
+Position FindMin( BinTree BST )
+{
+}
+Position FindMax( BinTree BST )
+{
+}
+
